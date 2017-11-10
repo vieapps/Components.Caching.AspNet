09e6888838c110d2c891d7e8be890fc35dfbef57
Quynh Nguyen <mr.quynh@gmail.com>
2017-11-11 03:54:16 +0700

    Release new version with Redis supported

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 //      Revision
 [assembly: AssemblyVersion("10.1.3.1712")]
 [assembly: AssemblyFileVersion("10.1.3.1712")]
-[assembly: AssemblyInformationalVersion("10.1 (v10.1.nstd-2017.12.01)")]
+[assembly: AssemblyInformationalVersion("10.1 (v10.1.nstd-2017)")]
